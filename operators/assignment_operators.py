cb5271040071a46f7f64f6c1f34d936bd6ee9e8a
Rajan Neupane <rajanneupane355@gmail.com>
2026-05-20 17:53:17 +0545

    learn about compund assignment , multiple assignment, and many more

---
+++ operators/assignment_operators.py
@@ -0,0 +1,67 @@
+"""
+assign means giving a value to a varible
+
+age = 15
+where,
+age -> variable
+= -> assignment_operator
+15-> value of variable
+
+we asign 15 to age
+"""
+
+#  we can assign different type of data to the variable
+a = 10
+name = "niijo"
+
+# we can change the value of varible by below method
+distance = 11.1
+# print(distance)
+distance = 123
+# print(distance)
+
+# we can assgin one varible to another variable
+a = 2
+c = 1
+# print(c)
+b = c
+# print(b)
+
+
+# multiple assignment 
+# jati variable xa teti nai value dina parene hunxa
+# a,b,c,d,e = 1,2,3,4
+# print(f'a={a}, b = {b}, c = {c}, d={d}')
+
+
+# giving same value to mutliple variables
+
+a=b= c= d =1
+# print(f'a={a}, b = {b}, c = {c}, d={d}')
+
+
+# compound assignment operators
+
+
+# addition
+# x = 1
+# # x = x+3
+# x += 3
+
+# x = y+x
+# x+=y
+
+
+# sub
+x = 4
+x = x -1
+x -=1
+print(x)
+# mul
+x = x*2
+x*=2
+
+# div 
+x /= 2
+# modular division
+x%=2
