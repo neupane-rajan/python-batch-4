c02a6adf6383130260d86910147fe607771a0ae8
Rajan Neupane <rajanneupane355@gmail.com>
2026-06-08 18:22:14 +0545

    integer operation completed

---
+++ basic/integer_operations.py
@@ -0,0 +1,36 @@
+
+# +,-,%,*,/,//,**,
+
+# string to integer conversion
+age = "25"
+age = int(age)
+
+# integer to string conversion
+num = 100
+# print(str(num))
+
+
+# useful integer functions
+
+# abs() convert negative number to postive number 
+# print(abs(-50))
+
+# pow()
+# print(pow(3,2))
+
+# round()
+# print(round(5.6))
+
+
+# getting last digit from a number
+
+num = 123456
+last_digit = num%10
+print(last_digit)
+
+# reverse a number
+
+num = 123
+convert = str(num)
+reverse = convert[::-1]
+print(reverse)
