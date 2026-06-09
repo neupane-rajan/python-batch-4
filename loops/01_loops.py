26418411e04e7df7680845450c1f1eda294b784f
Rajan Neupane <rajanneupane355@gmail.com>
2026-06-09 18:05:12 +0545

    rename loop file

---
+++ loops/01_loops.py
@@ -0,0 +1,35 @@
+# a loop is a programming structure that allow us to execute a block of code, multiple times.
+
+# range(end) :
+# range(start,end)
+# range(start,stop,step)
+# for i in range(5, -2, -1):
+#     print(i)
+
+
+# while loop
+# for loop
+
+"""
+syntax:
+
+while condition:
+    code...
+"""
+
+i = 1
+while i <=5:
+    print(i)
+    i += 1  # i = i + 1
+
+# print your name 5 times
+i = 1
+while i <=10:
+    print("rajan")
+    i = i+1 # i +=1
+
+
+# infinite loop 
+
+while True:
+    print("hello")
