3cf75ada25fde34a6e4b6f0f48606f6d60c72208
Rajan Neupane <rajanneupane355@gmail.com>
2026-06-16 20:50:45 +0545

    nestedloop

--- loops/nested_loop.py
+++ loops/nested_loop.py
@@ -1,1 +1,10 @@
-# Nested Loop: A loop inside another loop is called nested loop~
+# Nested Loop: A loop inside another loop is called nested loop
+
+
+# example(structure)
+
+for i in range(3):
+    for j in range(2):
+        print(i, j,i)
+
+
