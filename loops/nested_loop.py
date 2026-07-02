0d570adaa1715193db6db4b99bf41846b594aa43
Rajan Neupane <rajanneupane355@gmail.com>
2026-07-02 19:21:27 +0545

    loop is now near to the end

--- loops/nested_loop.py
+++ loops/nested_loop.py
@@ -1,10 +1,10 @@
-# Nested Loop: A loop inside another loop is called nested loop
-
+# Nested Loop: A loop inside another loop is called nested loop. The inner loop completes all its iteration for every single iteration of the outerloop
 
 # example(structure)
 
-for i in range(3):
-    for j in range(2):
-        print(i, j,i)
+# for outer in range(1, 4):
+#     for inner in range(1, 4):
+#         print(f"outer={outer}, inner = {inner}")
 
-
+for i in range(6):
+    print("*")
--- test.py
+++ test.py
@@ -1,4 +1,5 @@
-for i in range(1,6):
-    for j in range(1,6):
-        print(j,end=" ")
+
+for i in range(5):
+    for j in range(5):
+        print("*", end = " ")
     print()
