002824c8b480b53a3f773e01588881eb61d3349a
Rajan Neupane <rajanneupane355@gmail.com>
2026-07-22 18:31:50 +0545

    function started

---
+++ function/01_function.py
@@ -0,0 +1,50 @@
+# function
+# a function is a block of code that perform a specific task.Instead of writing same code again and again , we write it once inside a function and use it whenever we want/need it.
+
+
+# print("Welcom to Niijo Tech")
+# print("Learn python through our tutors")
+
+
+# print("Welcom to Niijo Tech")
+# print("learn python through our tutors")
+
+# type of function
+# 1. Built in function.
+# example: print(), type(),int(),len(),range()
+
+
+# user define function
+# syntax:
+"""
+def function_name():
+    code/statement
+
+"""
+
+
+def greeting():
+    print("Welcom to Niijo Tech")
+    print("Learn python through our tutors")
+
+
+# calling function
+# greeting()
+
+# greeting()
+
+
+def empty_function():
+    pass
+
+
+name = input("What is your name?: ")
+language = input("which language you want to study: ")
+
+
+def greeting(name,language):
+    print(f"hello {name} , congratulation for taking {language} course")
+
+
+
+greeting(name="rohit",language="hacking")
